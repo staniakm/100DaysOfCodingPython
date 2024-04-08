15397a86814de3da9b367f53170891f65330b33c
mstaniak <mariusz.staniak@gmail.com>
2024-04-08 21:58:55 +0200

    day1 replace values

--- variables.py
+++ variables.py
@@ -10,3 +10,23 @@ print("Hello " + name)
 
 print("Length of name is: ")
 print(len(name))
+
+# how to replace variables
+# lets assign values to "a" and "b"
+# and then replace then by places
+
+a = input("Provide value for 'a'")
+b = input("Provide value for 'b'")
+
+# now we have 2 variables
+print("a: " + a)
+print("b: " + b)
+
+# lets replace then
+temp = a
+a = b
+b = temp
+
+#######
+print("a: " + a)  # this time, to "a" we should have assigned value previously assigned to b
+print("b: " + b)  # this time, to "b" we should have assigned value previously assigned to a
