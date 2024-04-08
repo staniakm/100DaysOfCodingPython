d4d1b219d2c8847ffbd5a4febd9ab41f34faed5d
mstaniak <mariusz.staniak@gmail.com>
2024-04-08 21:54:18 +0200

    day1 string variable length function

--- variables.py
+++ variables.py
@@ -1,9 +1,12 @@
 name = input("What is your name?\n>")
 
-#name set by input
+# name set by input
 print("Hello " + name)
 
 # variable overwritten by new value
 name = "Joe"
 
 print("Hello " + name)
+
+print("Length of name is: ")
+print(len(name))
