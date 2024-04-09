c7330e67786e034b9fd92e25413ff07647cd09cf
mstaniak <mariusz.staniak@gmail.com>
2024-04-09 19:58:51 +0200

    day2 math operations

---
+++ day2/math_operations.py
@@ -0,0 +1,8 @@
+print("Sum: 3+8=" + str(3 + 8))
+print("Subtraction: 3-8=" + str(3 - 8))
+print("Multiplication: 2*3=" + str(2 * 3) + " and type is " + str(type(2 * 3)))
+print("Division: 6/2=" + str(6 / 2) + " and type is " + str(type(6 / 2)))
+print("Power of: 2**3=" + str(2 ** 3) + " and type is " + str(type(2 ** 3)))
+
+# operation order
+print(3 * 3 + 3 / 3 - 3)  # should be 7
