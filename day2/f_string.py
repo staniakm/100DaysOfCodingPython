286a53863698c51136d24e408206036c1f8eb191
mstaniak <mariusz.staniak@gmail.com>
2024-04-09 20:11:55 +0200

    day2 f-string casting

---
+++ day2/f_string.py
@@ -0,0 +1,8 @@
+score = 1
+height = 1.8
+isWinning = False
+
+print("Your score is: " + str(score) + " height: " + str(height) + " winning: " + str(isWinning))
+
+# f-string
+print(f"Your score is: {score}, height: {height}, winning: {isWinning}")
