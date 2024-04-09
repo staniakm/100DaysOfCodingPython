2ae9bb431bfff481d9ac404644faa38a827b44eb
mstaniak <mariusz.staniak@gmail.com>
2024-04-09 19:34:21 +0200

    day1 band name generator

---
+++ band_name_generator.py
@@ -0,0 +1,5 @@
+print("Welcome to band name generator")
+city = input("What is the name of the city that you grow up?\n")
+pet_name = input("What is your pet name?\n")
+
+print("Your band can be named: " + city + " " + pet_name)
