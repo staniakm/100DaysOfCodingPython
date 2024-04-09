ae500c0105d356c53da8c0d6550b29c2b8342fb2
mstaniak <mariusz.staniak@gmail.com>
2024-04-09 20:25:46 +0200

    day2 weeks in life

---
+++ day2/life_in_weeks_calculator.py
@@ -0,0 +1,6 @@
+age = int(input("What is your age?"))
+
+print(f"Your age is: {age}")
+# there are 52 weeks in year
+print(f"You already lived for {age * 52} weeks")
+print(f"We can assume that there left {(90 - age) * 52} weeks of life")
