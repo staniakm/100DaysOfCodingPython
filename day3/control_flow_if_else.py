f2e00222908569c2d952d596311b6c1cc482ef6e
mstaniak <mariusz.staniak@gmail.com>
2024-04-10 13:28:56 +0200

    day3 additional condition check

--- day3/control_flow_if_else.py
+++ day3/control_flow_if_else.py
@@ -1,9 +1,17 @@
 # to ride a rollecoster one have to be at least 120 cm
+# additional requirement, if age < 12 then one pay 7$ else 12$
+
 
 min_allowed_height = 120
+lower_cost_age = 12
 height = int(input("What is your height (in cm)?"))
 
 if height >= min_allowed_height:
     print("You can buy a ticket")
+    age = int(input("What is your age?"))
+    if age >= lower_cost_age:
+        print("Ticket cost is 12$")
+    else:
+        print("Ticket cost 7$")
 else:
     print("Your height is not enough")
