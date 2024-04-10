53689aaa4f292fedbe13bc466ad1ce571ec25583
mstaniak <mariusz.staniak@gmail.com>
2024-04-10 13:07:56 +0200

    day3 flow control

---
+++ day3/control_flow_if_else.py
@@ -0,0 +1,9 @@
+# to ride a rollecoster one have to be at least 120 cm
+
+min_allowed_height = 120
+height = int(input("What is your height (in cm)?"))
+
+if (height > min_allowed_height):
+    print("You can buy a ticket")
+else:
+    print("Your height is not enough")
