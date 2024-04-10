310e7d3b019a002e2c30c1845889b3391311b589
mstaniak <mariusz.staniak@gmail.com>
2024-04-10 21:18:20 +0200

    day3 improve bmi calculator

--- day2/bmi_calculator.py
+++ day2/bmi_calculator.py
@@ -6,3 +6,14 @@ height_in_m = float(input("What is your height (m)?"))
 bmi = weight_in_kg / (height_in_m ** 2)
 
 print("Your BMI is: " + str(int(bmi)))
+
+if bmi < 18.5:
+    print("Underweight")
+elif 18.5 <= bmi < 25:
+    print("Normal weight")
+elif 25 <= bmi < 30:
+    print("Slightly overweight")
+elif 30 <= bmi < 35:
+    print("Obese")
+else:
+    print("Critical obese")
