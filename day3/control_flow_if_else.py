42afd97647616adc84b059b49cd4b599e9a8e559
mstaniak <mariusz.staniak@gmail.com>
2024-04-10 13:20:20 +0200

    day3 change condition

--- day3/control_flow_if_else.py
+++ day3/control_flow_if_else.py
@@ -3,7 +3,7 @@
 min_allowed_height = 120
 height = int(input("What is your height (in cm)?"))
 
-if (height > min_allowed_height):
+if (height >= min_allowed_height):
     print("You can buy a ticket")
 else:
     print("Your height is not enough")
