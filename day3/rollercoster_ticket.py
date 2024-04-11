31545f0cb7e54aa25271d8b7ce73c0edc81ccdfb
mstaniak <mariusz.staniak@gmail.com>
2024-04-11 08:47:58 +0200

    day3 mid life crisis

--- day3/rollercoster_ticket.py
+++ day3/rollercoster_ticket.py
@@ -1,6 +1,7 @@
 # to ride a rollecoster one have to be at least 120 cm
 # additional requirement, if age < 12 then one pay 7$ else 12$
 # new requirement when age < 12 than cost is 5, between 12 and 18 7$ else 12$
+# midlife crisis - ppl 45-55 ride for free
 
 min_allowed_height = 120
 height = int(input("What is your height (in cm)?"))
@@ -13,6 +14,8 @@ if height >= min_allowed_height:
         ticket_price = 5
     elif 12 <= age <= 18:
         ticket_price = 7
+    elif age >= 45 and age <= 55:
+        ticket_price = 0
     else:
         ticket_price = 7
     photo = bool(input("Do you want photo?"))
