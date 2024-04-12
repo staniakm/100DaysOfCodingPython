ca205597e4db3f3d1cdfeb973f84726aa4c82165
mstaniak <mariusz.staniak@gmail.com>
2024-04-12 20:45:26 +0200

    day5 for loop calulations

--- day_5/loops.py
+++ day_5/loops.py
@@ -6,3 +6,27 @@ for fruit in fruits:
     print(fruit)
     print(fruit + " Pie")
 print("End of for loop")
+
+print("Practice")
+
+student_heights = input().split()
+print(len(student_heights))
+
+# convert to int
+for n in range(0, len(student_heights)):
+    student_heights[n] = int(student_heights[n])
+
+total = 0
+number = 0
+
+for student in student_heights:
+    total += student
+    number += 1
+
+average = total / len(student_heights)
+
+print(student_heights)
+
+print(f"Total height: {total}")
+print(f"Number of students: {number}")
+print(f"Average {average}")
