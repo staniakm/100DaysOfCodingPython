cd118a05b31716c54feecffc814989475f3148bd
mstaniak <mariusz.staniak@gmail.com>
2024-04-12 16:40:18 +0200

    day4 user list index to get index of letter

--- day_4/treasure_map.py
+++ day_4/treasure_map.py
@@ -3,16 +3,10 @@ line2 = [" ", " ", " "]
 line3 = [" ", " ", " "]
 
 treasure_map = [line1, line2, line3]
-
+abc = ["A", "B", "C"]
 result = list(input("Where do you want to hide treasure? (eg. B2)").upper())
 
-column = 0
-if (result[0] == "A"):
-    column = 0
-elif result[0] == "B":
-    column = 1
-else:
-    column = 2
+column = abc.index(result[0])
 row = int(result[1]) - 1
 
 treasure_map[row][column] = "X"
