bb07c0fa9dc20f7bc5c7d619a414bdd8bcd9ef5a
mstaniak <mariusz.staniak@gmail.com>
2024-04-13 10:31:27 +0200

    day8 function with params

---
+++ day_8/function_with_params.py
@@ -0,0 +1,15 @@
+def greet(name):
+    print("Hello")
+    print(name)
+    print("!!!")
+
+
+greet("Joe")
+
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"Great to see you from {location}")
+
+
+greet_with("Joe", "Poland")
