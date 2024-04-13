1827c6b093d0346743aaae4d07e1a6fdb603f4ed
mstaniak <mariusz.staniak@gmail.com>
2024-04-13 11:09:09 +0200

    day8 cesar cipher

---
+++ day_8/cesar_code.py
@@ -0,0 +1,38 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+            'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    encrypted = ""
+    for l in text:
+        encrypted += get_new_letter(l, shift)
+    return encrypted
+
+
+def get_new_letter(letter, shift_value):
+    if letter not in alphabet:
+        return letter
+    index = alphabet.index(letter)
+    new_index = index + shift_value
+    if new_index >= len(alphabet):
+        new_index = new_index - len(alphabet)
+    if new_index < 0:
+        new_index = len(alphabet) + new_index
+    return alphabet[new_index]
+
+
+def decrypt(text, shift):
+    decrypted = ""
+    for l in text:
+        decrypted += get_new_letter(l, -1 * shift)
+    return decrypted
+
+
+if (direction == "encode"):
+    print(encrypt(text=text, shift=shift))
+else:
+    print(decrypt(text=text, shift=shift))
