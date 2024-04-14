37aaa0fbc4972f927897dc8591d6dc27cc08e3d1
mstaniak <mariusz.staniak@gmail.com>
2024-04-14 21:42:28 +0200

    day14 higher lower game

---
+++ day14_higher_lower_game/game.py
@@ -0,0 +1,28 @@
+import random
+
+from art import logo, vs
+from gme_data import data
+
+print(logo)
+
+running = True
+correct_count = 0
+while running:
+    print("Who has more followers?")
+    first = random.choice(data)
+    second = random.choice(data)
+
+    print(f"1. {first['name']} - {first['description']}")
+    print(vs)
+    print(f"2. {second['name']} - {second['description']}")
+    answer = input()
+    if answer == "1" and first['follower_count'] > second['follower_count']:
+        correct_count += 1
+        print("Correct")
+    elif answer == '2' and first['follower_count'] < second['follower_count']:
+        correct_count += 1
+        print("Correct")
+    else:
+        print("Incorrect")
+        print(f"You get {correct_count} points")
+        running = False
