d999697f825b2352510624640fbffaa179660e7b
mstaniak <mariusz.staniak@gmail.com>
2024-04-14 10:08:50 +0200

    day10 use title function

--- day_10_functions/capitalize_name.py
+++ day_10_functions/capitalize_name.py
@@ -4,4 +4,9 @@ def capitalizer(f_name, l_name):
     return f"{name} {last_name}"
 
 
+def format_name(f_name, l_name):
+    return f"{f_name.title()} {l_name.title()}"
+
+
 print(capitalizer("joe", "monster"))
+print(format_name("joe", "MONSTER"))
