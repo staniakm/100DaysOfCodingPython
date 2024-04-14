f3af90ed430cdd5c0caabdcecaf5b77af0b4ab70
mstaniak <mariusz.staniak@gmail.com>
2024-04-14 21:45:23 +0200

    day14 improve second picker

--- day14_higher_lower_game/game.py
+++ day14_higher_lower_game/game.py
@@ -3,6 +3,14 @@ import random
 from art import logo, vs
 from gme_data import data
 
+
+def pick_second(first_selection):
+    second_pick = random.choice(data)
+    while first_selection == second_pick:
+        second_pick = random.choice(data)
+    return second_pick
+
+
 print(logo)
 
 running = True
@@ -10,8 +18,7 @@ correct_count = 0
 while running:
     print("Who has more followers?")
     first = random.choice(data)
-    second = random.choice(data)
-
+    second = pick_second(first)
     print(f"1. {first['name']} - {first['description']}")
     print(vs)
     print(f"2. {second['name']} - {second['description']}")
