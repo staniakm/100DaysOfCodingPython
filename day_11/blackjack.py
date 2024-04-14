26487107256c554ec7e79af3ee975f1ecf2d0d9d
mstaniak <mariusz.staniak@gmail.com>
2024-04-14 19:14:51 +0200

    day10 black jack

--- day_11/blackjack.py
+++ day_11/blackjack.py
@@ -18,22 +18,6 @@ import random
 ## Cards are not removed from the deck as they are drawn.
 ## The computer is the dealer.
 
-##################### Hints #####################
-
-# Hint 1: Go to this website and try out the Blackjack game:
-#   https://games.washingtonpost.com/games/blackjack/
-# Then try out the completed Blackjack project here:
-#   https://appbrewery.github.io/python-day11-demo/
-
-# Hint 2: Read this breakdown of program requirements:
-#   http://listmoz.com/view/6h34DJpvJBFVRlZfJvxF
-# Then try to create your own flowchart for the program.
-
-# Hint 3: Download and read this flow chart I've created:
-#   https://drive.google.com/uc?export=download&id=1rDkiHCrhaf9eX7u7yjM1qwSuyEk-rPnt
-
-# Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
-# 11 is the Ace.
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
 
@@ -41,70 +25,107 @@ def deal_card():
     return random.choice(cards)
 
 
-# Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-user_cards = []
-computer_cards = []
-
-
 def draw_card(player_cards, no_of_cards):
     for i in range(0, no_of_cards):
         player_cards.append(deal_card())
 
 
-draw_card(user_cards, 2)
-draw_card(computer_cards, 2)
-
-
-# Hint 6: Create a function called calculate_score() that takes a List of cards as input
-# and returns the score.
-# Look up the sum() function to help you do this.
 def calculate_score(user_list):
     cards_sum = sum(user_list)
     if cards_sum > 21:
-        user_list.remove(11)
-        user_list.append(1)
+        if user_list.count(11):
+            user_list.remove(11)
+            user_list.append(1)
         return sum(user_list)
-    elif cards_sum == 21:
-        return 0
     else:
         return cards_sum
 
 
-# print(f"User sum: {calculate_score(user_cards)}")
-# print(f"computer sum: {calculate_score(computer_cards)}")
-
-# Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
-
-# Hint 8: Inside calculate_score() check for an 11 (ace). If the score is already over 21, remove the 11 and replace it with a 1. You might need to look up append() and remove().
-
-# Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-
-computer_score = calculate_score(computer_cards)
-user_score = calculate_score(user_cards)
-
-if (computer_score == 0 or user_score == 0):
-    print("Game over")
-    if computer_score == 0:
-        print("Computer won")
+def check_blackjack(user_cards, computer_cards):
+    computer_score = calculate_score(computer_cards)
+    user_score = calculate_score(user_cards)
+    if len(computer_cards) == 2 and len(user_cards) == 2 and (computer_score == 0 or user_score == 0):
+        print("Black jack")
+        print(len(computer_cards))
+        if computer_score == 0:
+            print(f"Computer won with score {calculate_score(computer_cards)}")
+        else:
+            print(f"Player won with score {calculate_score(user_cards)}")
+        return True
     else:
-        print("Player won")
-# Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
-print(f"You have already: {user_score} points")
-answer = input("Do you want to draw another card (yes/no)?")
-if answer == "no":
+        return False
+
+
+def check_winning(user_cards, computer_cards):
+    computer_score = calculate_score(computer_cards)
+    user_score = calculate_score(user_cards)
+
     if computer_score > user_score:
         print("Computer won")
+        print(f"Player score: {user_score}\nComputer score: {computer_score}")
     elif computer_score < user_score:
         print("Player won")
+        print(f"Player score: {user_score}\nComputer score: {computer_score}")
     else:
         print("Draw")
-else:
-    draw_card(user_cards, 1)
+        print(f"Player score: {user_score}\nComputer score: {computer_score}")
 
-# Hint 11: The score will need to be rechecked with every new card drawn and the checks in Hint 9 need to be repeated until the game ends.
 
-# Hint 12: Once the user is done, it's time to let the computer play. The computer should keep drawing cards as long as it has a score less than 17.
+def check_overlap(total_cards):
+    return calculate_score(total_cards) > 21
 
-# Hint 13: Create a function called compare() and pass in the user_score and computer_score. If the computer and user both have the same score, then it's a draw. If the computer has a blackjack (0), then the user loses. If the user has a blackjack (0), then the user wins. If the user_score is over 21, then the user loses. If the computer_score is over 21, then the computer loses. If none of the above, then the player with the highest score wins.
 
-# Hint 14: Ask the user if they want to restart the game. If they answer yes, clear the console and start a new game of blackjack and show the logo from art.py.
+def reset_new_game(user_cards, computer_cards):
+    print("Preparing table...")
+    draw_card(user_cards, 2)
+    draw_card(computer_cards, 2)
+
+
+def start_game(new_game):
+    user_cards = []
+    computer_cards = []
+    reset_new_game(user_cards, computer_cards)
+    while new_game:
+        print("""
+        ############################ START NEW GAME ##################################
+        """)
+
+        if check_blackjack(user_cards, computer_cards):
+            answer = input("Do you want to play again?")
+            if answer == "no":
+                break
+        else:
+            next_card = True
+            while next_card and not check_blackjack(user_cards, computer_cards):
+                user_score = calculate_score(user_cards)
+                print(f"You have already: {user_score} points")
+                print(user_cards)
+                answer = input("Do you want to draw another card (yes/no)?")
+                if answer == "no":
+                    next_card = False
+                else:
+                    draw_card(user_cards, 1)
+
+            if check_overlap(user_cards):
+                print(f"Player lost with total {calculate_score(user_cards)}")
+            else:
+
+                while not check_blackjack(user_cards, computer_cards) and calculate_score(computer_cards) < 17:
+                    draw_card(computer_cards, 1)
+
+                print(f"Computer cards: {computer_cards}")
+                if check_overlap(computer_cards):
+                    print(f"Computer lost with score: {calculate_score(computer_cards)}")
+                else:
+                    check_winning(user_cards, computer_cards)
+
+            answer = input("Do you want to start again?")
+            if answer == "yes":
+                start_game(True)
+            else:
+                new_game = False
+
+
+start_game(True)
+
+print("Game over")
