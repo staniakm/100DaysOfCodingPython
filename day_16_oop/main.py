c7de52647ddbf2270fd4ca1447da1c993d90f4ff
mstaniak <mariusz.staniak@gmail.com>
2024-04-15 18:11:16 +0200

    day16 oop basics

---
+++ day_16_oop/main.py
@@ -0,0 +1,17 @@
+from turtle import Turtle, Screen
+
+
+timmy = Turtle()
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(250.0)
+timmy.left(45)
+timmy.forward(150.0)
+timmy.left(90)
+timmy.back(-100.0)
+timmy.right(90)
+timmy.backward(150)
+my_screen = Screen()
+
+my_screen.exitonclick()
+
