83a9454abf31c8662a92c3b220c6b092efbd7d8c
mstaniak <mariusz.staniak@gmail.com>
2024-04-16 07:57:17 +0200

    day17 oop class creation

--- day_17/oop_object_creation.py
+++ day_17/oop_object_creation.py
@@ -1,12 +1,14 @@
-#constructor
+# constructor
 
 class Car:
-    def __init__(self):
+    def __init__(self, mark, model, seats):
         print("Constructor triggered")
+        self.seats = seats
+        self.mark = mark
+        self.model = model
 
 
-
-car = Car()
-car.seats = 5
+car = Car(mark="Toyota", model="Corolla", seats=5)
 
 print(car.seats)
+print(car.mark)
